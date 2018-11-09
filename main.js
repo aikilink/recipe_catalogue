e3a7b88853d3f55bea2adc388fbe2b99a56f5bca
aikilink <quintinrg@gmail.com>
2018-11-09 12:41:04 -0600

    continued testing between HTML and JS, and added test functionality

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+var x
 var initialize = function() {
     x = document.getElementById("recipe_url").value;
     console.log(x);
+    document.getElementById("saved_link").innerHTML = x;
     return x
 };
+
+function show_x() {
+    console.log(x);
+};
+
+
+
 
