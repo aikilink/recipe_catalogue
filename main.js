14bf10de7409154ca123b01de0fd12cb5544e26a
aikilink <quintinrg@gmail.com>
2018-11-10 23:08:39 -0600

    started new functionality adding

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 var x
+var count = 1
 var initialize = function() {
     x = document.getElementById("recipe_url").value;
     console.log(x);
     document.getElementById("saved_link").innerHTML = x;
+    link_counter();
+    console.log(count);
     return x
+    
 };
 
-function show_x() {
+function log_x() {
     console.log(x);
 };
 
+function link_counter() {
+    count += 1;
+    return count;
+};
 
+/*add function to make new paragraphs*/
 
 
