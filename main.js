73a13a186337e5bcaa02a900c6dce09506f999e4
aikilink <quintinrg@gmail.com>
2018-11-13 12:13:55 -0600

    added appending function to insert new paragraphs

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 var x
-var count = 1
+var count = 0
 var initialize = function() {
     x = document.getElementById("recipe_url").value;
     console.log(x);
     document.getElementById("saved_link").innerHTML = x;
     link_counter();
     console.log(count);
+    insert_para();
     return x
-    
 };
 
-function log_x() {
-    console.log(x);
-};
+// function log_x() {
+//     console.log(x);
+// };
 
+//link_counter function doesn't do anything aside from incrementing var count, and logging count
 function link_counter() {
     count += 1;
     return count;
 };
 
-/*add function to make new paragraphs*/
-
 function insert_para() {
-    /*insert para into log_div/*
+    var para = document.createElement("p");
+    var node = document.createTextNode(x);
+    para.appendChild(node);
+    var new_element = document.getElementById("log_div");
+    new_element.appendChild(para);
 }
 
